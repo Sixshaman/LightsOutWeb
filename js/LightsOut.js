c63fb2e17ce26b072781b59890bb8639bbb94829
Sixshaman <Shaman1234@gmail.com>
2019-12-28 17:16:21 +0300

    sTYLE

--- js/LightsOut.js
+++ js/LightsOut.js
@@ -3829,7 +3829,7 @@ function main()
 
     function mainDraw()
     {
-        gl.clearColor(1.0, 1.0, 1.0, 0.0);
+        gl.clearColor(0.0, 0.0, 0.0, 0.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
 
         let drawFlags = 0;
